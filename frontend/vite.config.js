679c7019c0983cdbba470a4a8519045814103879
Charantayya <charantayya.cs25@bmsce.ac.in>
2025-11-28 00:06:43 +0530

    redirects added 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    build: {
+        outDir: "dist",
+    },
+    server: {
+        host: true,
+        port: 3000,
+    },
+    preview: {
+        host: "0.0.0.0",
+        port: 3000,
+    },
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
+});
